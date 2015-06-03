27d1aabf47aca37c628b3f55f9499e1f3a8f5647
rafalboguski <boguskirafal@gmail.com>
2015-06-03 14:29:43 +0200

    Configuration, maven generates now QEntity classes

---
+++ src/main/java/domain/Book.java
@@ -0,0 +1,59 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String title;
+
+    private String autor;
+
+    private int pages;
+
+
+    public Book(String title, String autor, int pages) {
+        this.title = title;
+        this.pages = pages;
+        this.autor = autor;
+    }
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
