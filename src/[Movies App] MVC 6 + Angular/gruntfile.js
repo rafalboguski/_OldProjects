e73b6209d3acd3c68fbdbd6f57da243d7f7179bd
Rafał Boguski <rafal.boguski@primaris.eu>
2015-09-09 16:49:45 +0200

    Grunt copy html/css files task confg

--- src/[Movies App] MVC 6 + Angular/gruntfile.js
+++ src/[Movies App] MVC 6 + Angular/gruntfile.js
@@ -3,12 +3,25 @@ module.exports = function (grunt) {
     // load Grunt plugins from NPM
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // configure plugins
     grunt.initConfig({
         uglify: {
             my_target: {
                 files: { 'wwwroot/app.js': ['Scripts/app.js', 'Scripts/**/*.js'] }
+            }
+        },
+        copy: {
+            files: {
+                cwd: 'Scripts/'  ,
+                src: '**/*.html',
+                dest: 'wwwroot',
+                expand: true
+                
+                
+          
+ 
             }
         },
 
