5c74b141d641bbb7e1863a399218777485bd3c00
rafalboguski <boguskirafal@gmail.com>
2016-04-12 21:58:16 +0200

    Storage initial commit

---
+++ Storage/Models/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rbog\Documents\Storage_Library.mdf;Integrated Security=True;Connect Timeout=30")
+        {
+
+        }
+
+        public static AppDbContext Create()
+        {
+            return new AppDbContext();
+        }
+
+        public DbSet<Star> Stars { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemVersion> ItemsVersions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Link> Links { get; set; }
+
+    }
+}
---
+++ Storage/Models/Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    // song, movie, book
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set; }
+
+        public bool Done { get; set; }
+        public bool ToFind { get; set; }
+        public bool ToFindBetter { get; set; }
+
+        public ICollection<ItemVersion> Versions { get; set; } = new List<ItemVersion>();
+        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+        public ICollection<Star> Stars { get; set; } = new List<Star>();
+
+        public DateTime? CreateDate { get; set; }
+        public DateTime? Timestamp { get; set; }
+    }
+
+    public class ItemVersion
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Site { get; set; }
+        public string Quality { get; set; }
+        public int FileSize { get; set; }
+        public int FileName { get; set; }
+        public bool Done { get; set; }
+        public int DoneLinks { get; set; }
+        public int Parts { get; set; }
+
+        public Item Item { get; set; }
+        public ICollection<Link> Links { get; set; } = new List<Link>();
+
+        public DateTime? CreateDate { get; set; }
+    }
+}
